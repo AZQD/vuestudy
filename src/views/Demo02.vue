b1d6622f98517dd6c930ce1be43d93bb31bc908d
AZQD <604972395@qq.com>
2021-03-20 23:54:49 +0800

    添加demo02

---
+++ src/views/Demo02.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="box">
+    Demo02
+  </div>
+</template>
+<script>
+  export default {
+
+    name: 'Demo02',
+
+    components: {},
+
+    data () {
+      return {}
+    },
+
+    created () {
+
+    },
+
+    methods: {},
+  }
+
+</script>
+
+<style lang="scss">
+
+  .box {
+
+  }
+
+</style>
