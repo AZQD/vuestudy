809a7498a2025b6a3f07e7a2386ae8a226307e5f
AZQD <604972395@qq.com>
2021-03-20 23:51:40 +0800

    完善about作为后续demo

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,31 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+  export default {
+
+    name: 'about',
+
+    components: {},
+
+    data () {
+      return {}
+    },
+
+    created () {
+
+    },
+
+    methods: {},
+  }
+
+</script>
+
+<style lang="scss">
+
+  .box {
+
+  }
+
+</style>
