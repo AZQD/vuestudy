ee1b23b27243121e55f9d6febc1c9f9abc57ea8d
AZQD <604972395@qq.com>
2021-03-20 16:36:29 +0800

    添加command组件

---
+++ src/views/Command.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="commandBox">
+    <p class="text">hello Command.vue</p>
+  </div>
+</template>
+<script>
+
+  export default {
+    name: 'Command',
+    components: {
+    }
+  }
+
+</script>
+
+<style lang="scss">
+  .commandBox {
+    .text {
+      color: pink;
+    }
+  }
+</style>
