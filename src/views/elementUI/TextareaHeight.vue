fcc775cfef43fd3b74f0582ee67289af13a206a8
AZQD <604972395@qq.com>
2021-08-05 22:41:52 +0800

    Textarea高度自适应补充

--- src/views/elementUI/TextareaHeight.vue
+++ src/views/elementUI/TextareaHeight.vue
@@ -1,23 +1,40 @@
 <template>
   <div class="block">
     <h3>学习重点：Textarea高度自适应</h3>
+
+    <!--固定高度-->
+    <el-input
+        type="textarea"
+        rows="2"
+        placeholder="请输入内容（固定高度）"
+        v-model="textarea1">
+    </el-input>
+
+    <div style="margin: 20px 0;"></div>
+
+    <!--高度自适应，有范围-->
+    <el-input
+        type="textarea"
+        :autosize="{ minRows: 2, maxRows: 6}"
+        placeholder="请输入内容（高度自适应，有范围）"
+        v-model="textarea2">
+    </el-input>
+
+    <div style="margin: 20px 0;"></div>
+
+    <!--高度自适应，没有范围-->
     <el-input
         type="textarea"
         autosize
-        placeholder="请输入内容"
-        v-model="textarea1">
+        placeholder="请输入内容（高度自适应，没有范围）"
+        v-model="textarea3">
     </el-input>
-    <div style="margin: 20px 0;"></div>
-    <el-input
-        type="textarea"
-        :autosize="{ minRows: 2, maxRows: 4}"
-        placeholder="请输入内容"
-        v-model="textarea2">
-    </el-input>
+
   </div>
 </template>
 
 <script>
+  //方式1：使用autosize；方式2：使用rows;
   export default {
 
     name: 'TextareaHeight',
@@ -25,7 +42,8 @@
     data () {
       return {
         textarea1: '',
-        textarea2: ''
+        textarea2: '',
+        textarea3: '',
       }
     },
 
