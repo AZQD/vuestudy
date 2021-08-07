695094aac8893b2261540312883ee9a2e0b99e94
AZQD <604972395@qq.com>
2021-08-07 16:19:00 +0800

    WangEditor基本使用

--- src/views/WangEditor.vue
+++ src/views/WangEditor.vue
@@ -23,7 +23,16 @@ import E from 'wangeditor'
 
     mounted () {
       const editor = new E('#div1');
+
       editor.config.height = 500; // 设置编辑区域高度为 500px（注意，先配置 height ，再执行 create()）
+      editor.config.zIndex = 500; // 层级
+      editor.config.placeholder = '自定义 placeholder 提示文字'; // placeholder
+      editor.config.focus = false; // 取消自动 focus
+      editor.config.onchange = function (html) {
+        console.log('获取编辑区内容方式1：', html); // 监控变化
+        console.log('获取编辑区内容方式2：', editor.txt.html()); // 任意地方可以使用
+      }
+
       editor.create() // 初始化逻辑需要在mounted中执行；
     },
 
