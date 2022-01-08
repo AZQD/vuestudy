23172f3a87c3376290140c69e4a88a4415a83687
AZQD <604972395@qq.com>
2022-01-08 19:19:03 +0800

    update

--- src/directive.js
+++ src/directive.js
@@ -1,19 +1,17 @@
 const install = function (Vue) {
-    // el-select组件数据过多，使用翻页加载数据
+    // el-select组件数据过多，使用翻页加载数据指令
     Vue.directive('el-select-loadmore', {
         bind(el, binding) {
             const SELECTWRAP_DOM = el.querySelector('.el-select-dropdown .el-select-dropdown__wrap');
             SELECTWRAP_DOM.addEventListener('scroll', function () {
-                // toFixed：把this.scrollTop转换为整数
+                // toFixed：把this.scrollTop转换为整数，兼容不同版本浏览器
                 const condition = this.scrollHeight - this.scrollTop.toFixed(0) <= this.clientHeight;
                 if (condition) binding.value();
             })
         }
     });
 }
-
 if (window.Vue) {
-    window.Vue.use(install);
+    Vue.use(install);
 }
-
-export default install
+export default install;
