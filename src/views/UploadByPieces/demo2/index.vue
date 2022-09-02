b7070e01b87570e1c8c90e2e418ef3f9cbd70f25
AZQD <604972395@qq.com>
2022-09-02 11:40:17 +0800

    å分片上传DEMO2组件应用

--- src/views/UploadByPieces/demo2/index.vue
+++ src/views/UploadByPieces/demo2/index.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
-    23
+    <el-card class="box-card">
+      <fileUpload ref="fileUpload" :ext="ext" :countLimit="5" :tip="tip">aaa</fileUpload>
+    </el-card>
   </div>
 </template>
 <script>
-export default {
-  data () {
-    return {
-      data: {}
+import fileUpload from './upload'
+export default{
+  name: 'hello',
+  components: {fileUpload},
+  data(){
+    return{
+      fileList: [],
+      ext: 'png,jpg,jpeg,mp3,mp4,pdf',
+      tip: '可上传png/jpg/jpeg/mp3/mp4/pdf，大小不超过200M'
     }
   },
+  created(){
+
+  },
   methods: {
+    getFileList: function(){
+      this.fileList = this.$refs.fileUpload.getFileList();
+      console.log(this.fileList);
+    }
   }
 }
 </script>
