51877a291dc99c594ef3e56dcee3dbaf5e12263a
AZQD <604972395@qq.com>
2023-02-02 10:04:21 +0800

    vue中使用@antv/x6显示流程图_设计-å¯简化è界面

--- src/views/AntvX6FlowDesign.vue
+++ src/views/AntvX6FlowDesign.vue
@@ -157,14 +157,6 @@ export default {
             title: '基础流程图',
             name: 'group1',
           },
-          {
-            title: '系统设计图',
-            name: 'group2',
-            graphHeight: 250,
-            layoutOptions: {
-              rowHeight: 70,
-            },
-          },
         ],
         layoutOptions: {
           columns: 2,
@@ -513,50 +505,6 @@ export default {
       })
       stencil.load([r1, r2, r3, r4, r5, r6], 'group1')
 
-      const imageShapes = [
-        {
-          label: 'Client',
-          image:
-              'https://gw.alipayobjects.com/zos/bmw-prod/687b6cb9-4b97-42a6-96d0-34b3099133ac.svg',
-        },
-        {
-          label: 'Http',
-          image:
-              'https://gw.alipayobjects.com/zos/bmw-prod/dc1ced06-417d-466f-927b-b4a4d3265791.svg',
-        },
-        {
-          label: 'Api',
-          image:
-              'https://gw.alipayobjects.com/zos/bmw-prod/c55d7ae1-8d20-4585-bd8f-ca23653a4489.svg',
-        },
-        {
-          label: 'Sql',
-          image:
-              'https://gw.alipayobjects.com/zos/bmw-prod/6eb71764-18ed-4149-b868-53ad1542c405.svg',
-        },
-        {
-          label: 'Clound',
-          image:
-              'https://gw.alipayobjects.com/zos/bmw-prod/c36fe7cb-dc24-4854-aeb5-88d8dc36d52e.svg',
-        },
-        {
-          label: 'Mq',
-          image:
-              'https://gw.alipayobjects.com/zos/bmw-prod/2010ac9f-40e7-49d4-8c4a-4fcf2f83033b.svg',
-        },
-      ]
-      const imageNodes = imageShapes.map((item) =>
-          graph.createNode({
-            shape: 'custom-image',
-            label: item.label,
-            attrs: {
-              image: {
-                'xlink:href': item.image,
-              },
-            },
-          }),
-      )
-      stencil.load(imageNodes, 'group2')
 // #endregion
 
       function preWork() {
