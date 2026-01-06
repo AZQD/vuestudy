cf067934accdd0bef2a3edf7f74dce84fca6ac42
AZQD <604972395@qq.com>
2026-01-06 14:45:58 +0800

    参考文档

--- src/views/contractReview.vue
+++ src/views/contractReview.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="box">
     <h3>学习重点：百度智能云 智能文档分析平台 - 合同审查</h3>
+    <p>参考文档：https://console.bce.baidu.com/textmind/application/textReview/sampleTask</p>
     <div id="jsonId">{{aaa}}</div>
   </div>
 </template>
